2883472f4bb4539ef83b28002dec69f25faa2de4
celes <celes@DESKTOP-43EPKPM>
2018-06-01 15:45:52 -0500

    tarea para blackboard

---
+++ src/abstraccion/Usuario.java
@@ -0,0 +1,29 @@
+
+package abstraccion;
+
+/**
+ *
+ * @author celes
+ */
+public class Usuario {
+    private float peso;
+    private float altura;
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+    
+    
+}
